2e30366d7615252525b3522ae924058c98c0edbd
miles0007 <milesstonner@gmail.com>
2021-05-29 23:15:57 +0530

    Port change

--- index.js
+++ index.js
@@ -76,5 +76,5 @@ io.on('connection', (socket) => {
     })
 })
 
-
-server.listen(8000, () => console.log("Listening on Port: 8000"))
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log("Listening on Port: 8000"))
